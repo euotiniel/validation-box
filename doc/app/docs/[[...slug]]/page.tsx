dfdd6de6280438f1574fc8acc13a4542968d229a
euotiniel <otonielemanuel21@gmail.com>
2025-02-24 20:50:46 +0100

    chore(doc): add link

--- doc/app/docs/[[...slug]]/page.tsx
+++ doc/app/docs/[[...slug]]/page.tsx
@@ -24,6 +24,7 @@ export default async function Page(props: {
   return (
     <DocsPage
       tableOfContent={{
+        enabled: true,
         style: "clerk",
       }}
       toc={page.data.toc}
